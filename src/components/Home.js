0706d07425f227b5ed064a399fc96040464f20da
Ryan Lee <djrlee51@gmail.com>
2018-04-02 09:12:22 -0700

    added new background and some verbage for services

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ class Home extends Component {
             </div>
           </div>
           <div>
-            <Carousel />
+            
           </div>
           <Services />
         </div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,7 +11,7 @@ class Services extends Component {
 	      		<h3 className="service">Customized Facial</h3><h3 className="price">$90</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">All facials are customized to the individual's specific skin care needs upon examination. Facials consist of deep cleansing, extractions as needed, masque and moisture. Light shoulder and arm massage included to create a serene experience.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -19,7 +19,7 @@ class Services extends Component {
 	      		<h3 className="service">Brow Wax Clean Up</h3><h3 className="price">$15</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">The Benefit brow wax is the quickest way to not only flatter but flaunt your favorite features. Whether you’re searching for eye-opening arches or a little extra lift in your overall look, the results from this service will leave you looking polished, pretty and put-together.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -27,7 +27,7 @@ class Services extends Component {
 	      		<h3 className="service">Brow Wax Design</h3><h3 className="price">$20</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">Brow waxing has become an art designed to create the best frame for your eyes. You are unique. Your face provides the answer to the best shape for your brows. Allow me to create the best frame for your eyes.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -35,7 +35,7 @@ class Services extends Component {
 	      		<h3 className="service">Lip Wax</h3><h3 className="price">$15</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">Lip waxing create's a clean and defined look. All types and color of hair are removed quickly and efficiently. Many clients enjoy the smooth and flawless application of makeup in the weeks following their face waxing appointment.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -43,7 +43,7 @@ class Services extends Component {
 	      		<h3 className="service">Nose/Chin Wax</h3><h3 className="price">$10 each</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">Nose/Chin waxing create a clean and defined look. All types and color of hair are removed quickly and efficiently. Many clients enjoy the smooth and flawless application of makeup in the weeks following their face waxing appointment.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -51,7 +51,7 @@ class Services extends Component {
 	      		<h3 className="service">Full Face Wax Including Brows</h3><h3 className="price">$40</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">Face the world with full confidence. Hair is waxed from forehead, cheeks, sideburns, lip, chin, and just under the jawbone. Also including brows and nose. For eyebrows, cheeks, and chin, this full facial waxing service provides a completely smooth foundation for flawless make-up application.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -59,7 +59,7 @@ class Services extends Component {
 	      		<h3 className="service">1/2 Leg Wax</h3><h3 className="price">$35</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A ½ Leg Wax includes the area on your legs from the top of the foot to the knee.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -67,7 +67,7 @@ class Services extends Component {
 	      		<h3 className="service">Full Leg Wax</h3><h3 className="price">$70</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A Full Leg Wax includes all of the hair growth from the top of the foot to the top of the thigh.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -75,7 +75,7 @@ class Services extends Component {
 	      		<h3 className="service">1/2 Arm Wax</h3><h3 className="price">$25</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A 1/2 Arm Wax includes shoulders to elbows or elbows to wrists. Includes hands.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -83,7 +83,7 @@ class Services extends Component {
 	      		<h3 className="service">Full Arm Wax</h3><h3 className="price">$50</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A Full Arm Wax includes shoulders to wrists, includes hands.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -91,7 +91,7 @@ class Services extends Component {
 	      		<h3 className="service">Bikini Wax</h3><h3 className="price">$40</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A Bikini Wax includes the removal of all hair along the outside of your underwear or bathing suit line.</p>
 	      	</div>
 	    </div>
 	    <div className="serviceContainer">
@@ -99,7 +99,7 @@ class Services extends Component {
 	      		<h3 className="service">Brazilian</h3><h3 className="price">$65</h3>
 	      	</div>
 	      	<div className="descriptionContainer">
-	      		<p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	      		<p className="description">A Brazilian Wax includes full intimate area wax including between the cheeks.</p>
 	      	</div>
 	    </div>
       </div>
